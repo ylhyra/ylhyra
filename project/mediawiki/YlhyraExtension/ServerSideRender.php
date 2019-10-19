967babe4e34244ac94f69566573499291c112c5c
Egill <>
2019-10-19 14:47:42 +0000

    no message

--- project/mediawiki/YlhyraExtension/ServerSideRender.php
+++ project/mediawiki/YlhyraExtension/ServerSideRender.php
@@ -13,31 +13,31 @@ $wgExtensionCredits['ylhyra_backend_parse'][] = array(
 // Register hooks
 $wgHooks['ParserAfterTidy'][] = function( Parser &$parser, &$text ) {
   return true;
-  // if (strpos($text, 'data-document-start') !== false) {
-  //   if(function_exists('curl_version')) { // Check if CURL is installed
-  //     $url = 'http://localhost:9123/api/render';
-  //     $timeout=2;
-  //     $payload = json_encode(array(
-  //       'html' => $text
-  //     ));
-  //     $ch = curl_init($url);
-  //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  //     curl_setopt($ch, CURLINFO_HEADER_OUT, false);
-  //     curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-  //     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-  //     curl_setopt($ch, CURLOPT_POST, true);
-  //     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-  //     curl_setopt($ch, CURLOPT_FAILONERROR, true);
-  //     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  //       'Content-Type: application/json',
-  //       'Content-Length: ' . strlen($payload))
-  //     );
-  //     $result = curl_exec($ch);
-  //     curl_close($ch);
-  //     if($result) {
-  //       $text = $result;
-  //     }
-  //   }
-  // }
-  // return true;
+  if (strpos($text, 'data-document-start') !== false) {
+    if(function_exists('curl_version')) { // Check if CURL is installed
+      $url = 'http://localhost:9123/api/render';
+      $timeout=2;
+      $payload = json_encode(array(
+        'html' => $text
+      ));
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLINFO_HEADER_OUT, false);
+      curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+      curl_setopt($ch, CURLOPT_FAILONERROR, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($payload))
+      );
+      $result = curl_exec($ch);
+      curl_close($ch);
+      if($result) {
+        $text = $result;
+      }
+    }
+  }
+  return true;
 };
