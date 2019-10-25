af55e96377c7588756ee3642705094ff32cb947c
Egill <>
2019-10-25 11:04:34 +0000

    no message

--- project/text-plugin/Render/Audio/AudioClip.js
+++ project/text-plugin/Render/Audio/AudioClip.js
@@ -8,12 +8,12 @@ const AudioClip = {
   play: (sound_files) => {
     audio && audio.pause()
     if (Array.isArray) {
-      audio = new Audio(sound_files[0])
+      audio = new Audio(sound_files[0]) 
     } else {
       audio = new Audio(sound_files)
     }
     const promise = audio.play()
-    if (promise) { 
+    if (promise) {
       promise.catch(() => {
         // console.warn('Audio not played since user has not yet interacted with document')
       })
