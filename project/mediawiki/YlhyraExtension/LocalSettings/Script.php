5b3a93144fdcfee9b91cbc2b343ed27e4b7b2b91
Egill <>
2019-10-25 14:09:52 +0000

    no message

--- project/mediawiki/YlhyraExtension/LocalSettings/Script.php
+++ project/mediawiki/YlhyraExtension/LocalSettings/Script.php
@@ -1,15 +1,18 @@
 <?php
 require_once "$IP/extensions/HeadScript/HeadScript.php";
 
+
 /*
   To use Ylhýra development mode, run the script in setDevelopmentMode.js
 */
 $wgHeadScriptCode = <<<'START_END_MARKER'
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type='text/javascript' src='https://app.ylhyra.is/app.js?build=000000'></script>
 START_END_MARKER;
 
 if ($_COOKIE and $_COOKIE['development']=='true') {
   $wgHeadScriptCode = <<<'START_END_MARKER'
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type='text/javascript' src='https://localhost:8000/app.js'></script>
 START_END_MARKER;
 }
