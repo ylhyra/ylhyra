087b86fdd67a7555cc89d00268b8c6971f6b2a2c
Egill Sigurður <egillsigurdur@gmail.com>
2019-12-08 13:49:10 +0000

    no message

--- project/mediawiki/YlhyraExtension/LocalSettings/Script.php
+++ project/mediawiki/YlhyraExtension/LocalSettings/Script.php
@@ -10,7 +10,7 @@ $wgHeadScriptCode = <<<'START_END_MARKER'
 <script type='text/javascript' src='https://app.ylhyra.is/app.js?build=000000'></script>
 START_END_MARKER;
 
-if ($_COOKIE and $_COOKIE['development']=='true') {
+if ($_COOKIE and isset($_COOKIE['development']) and $_COOKIE['development']=='true') {
   $wgHeadScriptCode = <<<'START_END_MARKER'
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type='text/javascript' src='https://localhost:8000/app.js'></script>
