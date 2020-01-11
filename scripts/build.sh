b898a9f73003e2f0285cbf2c2eb67c89f1209a59
Egill <>
2020-01-11 16:46:26 +0000

    no message

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ cd ${BASH_SOURCE%/*}/../
 export NODE_ENV=production
 if node ./scripts/webpack/frontend/build.js; then
   [ -d ./build ] && mv ./build ./build_old
-  mv ./build_temp ./build
+  mv ./build_temp ./build 
   [ -d ./build_old ] && rm -rf ./build_old
 fi
