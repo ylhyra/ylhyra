871f2fb4f9cd1a593aaeea554f15f69cc022412c
Egill <>
2020-01-31 13:05:25 +0000

    no message

---
+++ project/mediawiki/maintenance/sitemap.sh
@@ -0,0 +1,1 @@
+php maintenance/generateSitemap.php --memory-limit=50M --fspath=/home/egill/mediawiki/sitemap/ --identifier=ylhyra --urlpath=sitemap --server=https://ylhyra.is --compress=no --skip-redirects
