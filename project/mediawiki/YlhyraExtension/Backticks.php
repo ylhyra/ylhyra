0e3d2429fff64d18ed930bf41c2f3d9077f47bd2
Egill <>
2020-02-15 23:57:56 +0000

    no message

--- project/mediawiki/YlhyraExtension/Backticks.php
+++ project/mediawiki/YlhyraExtension/Backticks.php
@@ -15,7 +15,7 @@ $wgExtensionCredits['ylhyra_backticks'][] = array(
 
 // Register hooks
 $wgHooks['InternalParseBeforeLinks'][] = function( &$parser, &$text, &$stripState ) {
-  $text = preg_replace("/`(.*?)`/", "{{translate|$1}}",$text);
+  // $text = preg_replace("/`(.*?)`/", "{{translate|$1}}",$text);
   // $text = preg_replace("/`(.*?)`/", "<span class='icelandic'>$1</span>",$text);
   // $text = preg_replace('/([^\\\\]|^)`([^`]*)`/', '$1{{translate|$2}}', $text);
   // $text = preg_replace('/\\\\\`/', '`', $text);
