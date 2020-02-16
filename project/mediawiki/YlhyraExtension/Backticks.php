2e08e615871ff2368ef4b750cb3032ffdc023192
Egill <>
2020-02-16 00:13:57 +0000

    no message

--- project/mediawiki/YlhyraExtension/Backticks.php
+++ project/mediawiki/YlhyraExtension/Backticks.php
@@ -1,10 +1,15 @@
 <?php
 
-// Register hooks
-$wgHooks['ParserBeforeStrip'][] = function( &$parser, &$text, &$stripState ) {
-  // $text = preg_replace("/`(.*?)`/", "{{translate|$1}}",$text);
-  $text = preg_replace("/`(.*?)`/", "<span class='icelandic'>$1</span>",$text);
-  // $text = preg_replace('/([^\\\\]|^)`([^`]*)`/', '$1{{translate|$2}}', $text);
-  // $text = preg_replace('/\\\\\`/', '`', $text);
+// Converts text inside backticks to "<span class=icelandic></span>".
+// Author: User:Sk4p on https://www.mediawiki.org/wiki/Extension_talk:BacktickCode
+$wgHooks['InternalParseBeforeLinks'][] = function( &$parser, &$text, &$stripState ) {
+  $fixprefix = preg_replace('/`/', '~', Parser::MARKER_PREFIX);
+  $fixsuffix = preg_replace('/`/', '~', Parser::MARKER_SUFFIX);
+  $text = str_replace(Parser::MARKER_PREFIX, $fixprefix, $text);
+  $text = str_replace(Parser::MARKER_SUFFIX, $fixsuffix, $text);
+  $text = preg_replace('/([^\\\\]|^)`([^`\x7f]*)`/', '$1<span class="icelandic">$2</span>', $text);
+  $text = preg_replace('/\\\\\`/', '`', $text);
+  $text = str_replace($fixprefix, Parser::MARKER_PREFIX, $text);
+  $text = str_replace($fixsuffix, Parser::MARKER_SUFFIX, $text);
   return true;
 };
