f91433cf87310fd8356e6f019c9aa686fe69dc02
Egill <egill@egill.xyz>
2020-06-29 15:56:39 +0000

    no message

--- mediawiki/settings/Script.php
+++ mediawiki/settings/Script.php
@@ -2,21 +2,16 @@
 /*
   To use Ylhýra development mode, run the script in setDevelopmentMode.js
 */
-$scriptToLoad = <<<'START_END_MARKER'
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script type='text/javascript' src='https://app.ylhyra.is/app.js?build=000000'></script>
-START_END_MARKER;
+$script = "https://app.ylhyra.is/app.js?build=000000";
 
 if ($_COOKIE and isset($_COOKIE['development']) and $_COOKIE['development']=='true') {
-  $scriptToLoad = <<<'START_END_MARKER'
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script type='text/javascript' src='https://localhost:8000/app.js'></script>
-START_END_MARKER;
+  $script = "https://localhost:8000/app.js";
 }
 
 $wgHooks['BeforePageDisplay'][] = 'LoadScriptInHeader';
 function LoadScriptInHeader(OutputPage &$out, Skin &$skin) {
-  global $scriptToLoad;
-	$out->addHeadItem('', $scriptToLoad );
+  global $script;
+  $out->addHeadItem('viewport', '<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+	$out->addHeadItem('ylhyra', '<script type="text/javascript" src="'.$script.'"></script>');
 	return true;
 }
