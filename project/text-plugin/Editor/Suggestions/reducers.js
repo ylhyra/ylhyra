5564ed714843fc6cb4522d57673659183277da60
Egill <egill@egill.xyz>
2020-07-08 12:37:29 +0000

    no message

--- project/text-plugin/Editor/Suggestions/reducers.js
+++ project/text-plugin/Editor/Suggestions/reducers.js
@@ -1,8 +1,9 @@
 const suggestions = (state = {}, action) => {
   let update = {}
   switch (action.type) {
-    case 'LOAD_EDITOR':
-      return action.content.suggestions || {}
+    case 'TOKENIZED':
+      // console.warn(action)
+      return action.currentDocumentData.suggestions || {}
     case 'SUGGEST':
       /* Suggest translation */
       Object.keys(action.definitions).forEach(id => {
