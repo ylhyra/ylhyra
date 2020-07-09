34239a0911f333402713a67970bc1404fa9c647d
Egill <egill@egill.xyz>
2020-07-09 19:07:53 +0000

    no message

--- project/frontend/Render/Elements/Inflection/Noun.js
+++ project/frontend/Render/Elements/Inflection/Noun.js
@@ -2,26 +2,32 @@ import React from 'react'
 import { Word } from './object'
 
 const Noun = (word) => {
-  return table([
-    null, <th rowspan="2">Singular</th>,
-    null, <th>Without article</th>, <th>With article</th>,
-    <th>Nominative</th>, word.get('singular', 'without-article', 'nominative'), word.get('singular', 'with-article', 'nominative'),
-    <th>Accusative</th>, word.get('singular', 'without-article', 'accusative'), word.get('singular', 'with-article', 'accusative'),
-    <th>Dative</th>, word.get('singular', 'without-article', 'dative'), word.get('singular', 'with-article', 'dative'),
-    <th>Genitive</th>, word.get('singular', 'without-article', 'genitive'), word.get('singular', 'with-article', 'genitive'),
-  ])
+  return [
+    table(FlipColumnsAndRows([
+      [<th colSpan="3">Singular</th>, null, <th>Nominative</th>, <th>Accusative</th>, <th>Dative</th>, <th>Genitive</th>,],
+      [null, <th>Without article</th>, ...word.get('singular', 'without-article').getCases(),],
+      [null,                               <th>With article</th>, ...word.get('singular', 'with-article').getCases(),],
+    ])),
+    table(FlipColumnsAndRows([
+      [null, null, <th>Nominative</th>, <th>Accusative</th>, <th>Dative</th>, <th>Genitive</th>,],
+      [<th colSpan="2">Plural</th>, <th>Without article</th>,...word.get('plural', 'without-article').getCases(),],
+      [null,                               <th>Without article</th>, ...word.get('plural', 'with-article').getCases(),],
+    ]))
+  ]
 }
 
 
 const table = (input) => (
   <table className="wikitable">
     <tbody>
-      {table.map(row => (
+      {input.map(row => (
         <tr>
           {row.map(cell => {
             if(cell instanceof Word) {
               return <td>{cell.renderCell()}</td>
-            } else {
+            } else if (cell === null) {
+              return <th/>
+            }else {
               return cell
             }
           })}
@@ -31,17 +37,16 @@ const table = (input) => (
   </table>
 )
 
-
 export default Noun
 
 
 /* Chuan Sun https://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript */
-const transposeArray = (mat) => {
+const FlipColumnsAndRows = (input) => {
   let newMat = [];
-  for (let j = 0; j < mat[0].length; j++) { // j are columns
+  for (let j = 0; j < input[0].length; j++) { // j are columns
     let temp = [];
-    for (let i = 0; i < mat.length; i++) { // i are rows
-      temp.push(mat[i][j]); // so temp will be the j(th) column in mat
+    for (let i = 0; i < input.length; i++) { // i are rows
+      temp.push(input[i][j]); // so temp will be the j(th) column in mat
     }
     newMat.push(temp); // then just push every column in newMat
   }
