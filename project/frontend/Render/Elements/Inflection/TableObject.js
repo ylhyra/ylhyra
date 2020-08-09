ba481f9a8e24484d2fe538f90ffc3c23b16b1a7d
Egill <egill@egill.xyz>
2020-08-09 19:16:06 +0000

    no message

--- project/frontend/Render/Elements/Inflection/TableObject.js
+++ project/frontend/Render/Elements/Inflection/TableObject.js
@@ -10,8 +10,8 @@ const IterateOver = (row, word) => {
   let table = null
   if (word.is('adjective') && ['singular', 'plural'].includes(row.tag)) {
     table = GenerateTable(row.values, {
-      columns: ['masculine', 'feminine', 'neuter'],
-      rows: ['nominative', 'accusative', 'dative', 'genitive']
+      column_names: ['masculine', 'feminine', 'neuter'],
+      row_names: ['nominative', 'accusative', 'dative', 'genitive']
     })
   }
   return <div className="indent">
@@ -27,40 +27,18 @@ const IterateOver = (row, word) => {
 
 /* Expects nested array of Columns -> Rows -> Values */
 const GenerateTable = (input, structure) => {
-  const { columns, rows } = structure
+  const { column_names, row_names } = structure
   let word = (new Word()).importTree(input)
-  let table = new Table()
-  //
-  // console.warn(input)
-  // console.log(word)
-  // word.get('masculine')
-  // return;
-
-  let t = []
-  columns.forEach((column, column_index) => {
-    let c = []
-    rows.forEach((row, row_index) => {
-      // console.log(word.get(column, row))
-      c.push(word.get(column, row))
+  let table = []
+  row_names.forEach((row_name, row_index) => {
+    let column = []
+    column_names.forEach((column_name, column_index) => {
+      column.push(word.get(column_name, row_name))
     })
-    t.push(c)
+    table.push(column)
   })
-  console.log(t)
-  return TableHTML(t)
-
-  // column_array.forEach(i => {
-  //   table.addColumn({
-  //     title: i.tag
-  //   })
-  // })
-  // TableHTML(column_array)
+  return TableHTML(table)
 }
-
-class Table {
-  constructor() {}
-}
-
-
 
 const TableHTML = (input, highlight = []) => {
   return (
@@ -84,22 +62,3 @@ const TableHTML = (input, highlight = []) => {
     </table>
   )
 }
-
-/* Chuan Sun https://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript */
-const FlipColumnsAndRows = (input) => {
-  let newMat = [];
-  for (let j = 0; j < input[0].length; j++) { // j are columns
-    let temp = [];
-    for (let i = 0; i < input.length; i++) { // i are rows
-      temp.push(input[i][j]); // so temp will be the j(th) column in mat
-    }
-    newMat.push(temp); // then just push every column in newMat
-  }
-  return newMat;
-}
-
-// return TableHTML(FlipColumnsAndRows([
-//   [null, <th>{link('Nominative')}</th>, <th>{link('Accusative')}</th>, <th>{link('Dative')}</th>, <th>{link('Genitive')}</th>, ],
-//   [<th colSpan="2">{link('Without definite article')}</th>, ...word.get('without definite article').getCases(), ],
-//   [<th colSpan="2">{link('With definite article')}</th>, ...word.get('with definite article').getCases(), ],
-// ]))
