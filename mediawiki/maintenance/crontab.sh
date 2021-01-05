2f50a91f798a207a5cab175a0de045ae8e4b6320
Egill <egill@egill.xyz>
2021-01-05 21:09:32 +0000

    no message

--- mediawiki/maintenance/crontab.sh
+++ mediawiki/maintenance/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 php maintenance/fixDoubleRedirects.php
-rm -rf images/cache/
+rm -rf images/cache/*
 php maintenance/rebuildFileCache.php
 php maintenance/runJobs.php --maxtime=3600
 # php maintenance/storage/compressOld.php
