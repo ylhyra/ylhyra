d97e21b41604ace28779d93cfc92fab8afc525ef
Egill <egill@egill.xyz>
2021-04-03 14:25:10 +0000

    no message

--- project/frontend/Vocabulary/actions.js
+++ project/frontend/Vocabulary/actions.js
@@ -5,7 +5,8 @@ export const BAD = 1
 export const OK = 2
 export const PERFECT = 3
 
-let counter = 0
+let numberOfCardsSeen = 0
+let positionInQueue = 0
 let lastSeenBelongsTo = {}
 let currentCard = null
 
@@ -14,17 +15,18 @@ let currentCard = null
   status - Status of card in the current study session, shown in progress bar
 */
 class Card {
-  constructor(data) {
+  constructor(data, index) {
     Object.assign(this, data)
     this.rating = null
     this.ratingHistoryForSession = []
+    this.nextShow = index + numberOfCardsSeen
   }
   get() {
     return this.data
   }
   rate(rating) {
     this.ratingHistoryForSession.unshift(rating)
-    this.lastSeen = counter
+    this.lastSeen = numberOfCardsSeen
 
     this.status = rating
     this.rating = average(this.ratingHistoryForSession.slice(0, 2))
@@ -37,12 +39,15 @@ class Card {
     } else if (rating === PERFECT) {
       next = 16
     }
-    this.nextShow = counter + next
+    this.nextShow = numberOfCardsSeen + next
     // if(this.ratingHistoryForSession.slice(0, 2))
   }
+  getRawRanking(){
+    return this.nextShow - positionInQueue
+  }
   getRanking() {
-    let ranking = this.rating || OK
-    const ticksSinceSeen = counter - lastSeenBelongsTo[this.belongs_to]
+    let ranking = this.getRawRanking()
+    const ticksSinceSeen = positionInQueue - lastSeenBelongsTo[this.belongs_to]
     if (ticksSinceSeen < 4) {
       return ranking + 100;
     }
@@ -65,7 +70,7 @@ class Deck {
     // cards_input.forEach(card => {
     //   id_to_card[card.id] = card
     // })
-    this.cards = cards.map(card => new Card(card))
+    this.cards = cards.map((card, index) => new Card(card, index))
     this.next()
     // /* New cards must be studied in the correct order */
     // this.newCards = this.cards.filter(card => card.isNew())
@@ -77,18 +82,24 @@ class Deck {
     return currentCard
   }
   rateCard(rating) {
-    currentCard.rate(rating, counter)
+    currentCard.rate(rating)
   }
   next() {
-    currentCard = this.cards.sort((a, b) => a.getRanking() - b.getRanking())[0]
-    counter++;
-    lastSeenBelongsTo[currentCard.belongs_to] = counter
-    // console.log(currentCard)
-    console.log(this.cards
-      .sort((a, b) => a.rating - b.rating)
-      .map(i => `${i.rating}\t${i.from==='is'?i.is:i.en}`)
+    const ranked = this.cards.sort((a, b) => a.getRanking() - b.getRanking())
+    console.log(this.cards.sort((a, b) => a.getRawRanking() - b.getRawRanking())
+      .map(i => `${i.getRawRanking()}\t${i.getRanking()}\t${i.from==='is'?i.is:i.en}`)
       .join('\n')
     )
+    console.log(ranked.slice(0,4))
+    currentCard = ranked[0]
+    numberOfCardsSeen++;
+    let shouldIncreaseAdjustedCounter = this.cards.filter(i => i.getRawRanking() < 5).length < 5
+    console.log({ shouldIncreaseAdjustedCounter })
+    if (shouldIncreaseAdjustedCounter) {
+      positionInQueue++;
+    }
+    lastSeenBelongsTo[currentCard.belongs_to] = numberOfCardsSeen
+    // console.log(currentCard)
   }
   getStatus() {
     return {
@@ -108,7 +119,7 @@ export const load = () => {
     type: 'LOAD_CARD',
     content: {
       ...deck.getCard(),
-      counter: deck.counter,
+      numberOfCardsSeen: deck.numberOfCardsSeen,
       status: deck.getStatus(),
     }
   })
