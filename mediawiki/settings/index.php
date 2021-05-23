a17c0dede3dfa3a5602ae898164193c67bd576ba
Egill <egill@egill.xyz>
2021-05-23 12:40:41 +0000

    no message

--- mediawiki/settings/index.php
+++ mediawiki/settings/index.php
@@ -5,6 +5,10 @@ if (!defined('MEDIAWIKI')) {
     exit;
 }
 
+if($_SERVER['HTTP_HOST'] == 'login.ylhyra.is') {
+  $IS_USER_LOGIN_SITE = true
+}
+
 error_reporting( -1 );
 ini_set( 'display_errors', 1 );
 
@@ -24,14 +28,18 @@ $wgShowDBErrorBacktrace = true;
 require_once(__DIR__.'/LocalSettings.php');
 require_once(__DIR__.'/Extensions.php');
 require_once(__DIR__.'/Namespaces.php');
-require_once(__DIR__.'/Userrights.php');
-require_once(__DIR__.'/VisualEditor.php');
-require_once(__DIR__.'/Script.php');
-require_once(__DIR__.'/Uploads.php');
 require_once(__DIR__.'/Email.php');
+if(!$IS_USER_LOGIN_SITE){
+  require_once(__DIR__.'/Userrights.php');
+  require_once(__DIR__.'/VisualEditor.php');
+  require_once(__DIR__.'/Uploads.php');
+  require_once(__DIR__.'/Script.php');
+  require_once(__DIR__.'/../ylhyra-extensions/ServerSideRender.php');
+  require_once(__DIR__.'/../ylhyra-extensions/Backticks.php');
+} else {
+  require_once(__DIR__.'/../user-login/index.php');
+}
 
-require_once(__DIR__.'/../ylhyra-extensions/ServerSideRender.php');
-require_once(__DIR__.'/../ylhyra-extensions/Backticks.php');
 // require_once(__DIR__.'/../ylhyra-extensions/VerifyLogin.php');
 
 
---
+++ mediawiki/user-login/index.php
@@ -0,0 +1,1 @@
+$wgServer = "https://login.ylhyra.is";
