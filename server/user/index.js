f9d883ffcbab9f35bd6e56b4a4353eaf602e3548
Egill <egill@egill.xyz>
2021-06-07 14:10:19 +0000

    magiclink

--- server/user/index.js
+++ server/user/index.js
@@ -3,41 +3,38 @@ const router = (require('express')).Router()
 import shortid from 'shortid'
 import sql from 'server/database/functions/SQL-template-literal'
 import cors from 'cors'
-import verifySession from 'server/VerifyMediawikiSession'
 import stable_stringify from 'json-stable-stringify'
-import sha256 from 'js-sha256'
-// import scrypt from 'scrypt-js'
-// const Cryptr = require('cryptr');
-// const cryptr = new Cryptr(process.env.COOKIE_SECRET || 'secret');
+import send_email from 'server/user/send_email'
 const key = process.env.COOKIE_SECRET || 'secret'
+var crypto = require('crypto');
 
 router.post('/user', async(req, res) => {
-  const session_verification_token = req.body.token
-  const user = await verifySession(session_verification_token, true)
-  if (!user) {
-    return res.status(400).send('Failed verification. Please <a href="mailto:ylhyra@ylhyra.is">contact support</a>.')
-  }
-  const { userid, name } = user
-  const data = {
-    userid,
-    name,
-    timestamp: (new Date()).getTime(),
-  }
-  const signature = sha256.hmac(key, stable_stringify(data))
-
-  // req.session.user = data
+  const {
+    username,
+    email,
+    password,
+  } = req.body
 
   return res.send({
-    user: data,
-    signature,
+    user: ':)'
   })
 })
 
-export const GetUser = (data) => {
-  if (!data) return;
-  const { user, signature } = data
-  if (sha256.hmac(key, stable_stringify(user)) !== signature) return;
-  return user;
-}
+router.get('/user', async(req, res) => {
+  const { email } = req.query
+  const short_token = ('0000' + parseInt(crypto.randomBytes(2).toString('hex'), 16).toString()).slice(-4)
+  const long_token = crypto.randomBytes(6).toString('hex')
+
+  return res.send({ short_token, long_token })
+})
+
+// router.get('/user', async(req, res) => {
+//   send_email()
+//
+//   return res.send({
+//     user: ':)'
+//   })
+// })
+
 
 export default router;
--- src/User/User/screens/Signup.js
+++ src/User/User/screens/Signup.js
@@ -2,37 +2,66 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { urls } from 'User/Routes/router'
 import HCaptcha from '@hcaptcha/react-hcaptcha'
-export default () => (
-  <div>
-    <Link to={urls.LOG_IN}>Already have an account?</Link>
+import axios from 'axios'
 
-    <div>
-      Step 1: Create an account<br/>
-      Step 2: Pay what you want
-    </div>
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleChange = (name, event) => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  }
+  handleSubmit = async(event) => {
+    console.log(this.state)
+    event.preventDefault();
 
-    <h2>Create an account</h2>
-    An account allows you to save your vocabulary progress and continue the game on other devices.
-    <form method="post">
-      <label>
-        <div>Choose a username:</div>
-        <input type="text"/>
-      </label>
-      <label>
-        <div>What is your email? (optional):</div>
-        <input type="text"/>
-      </label>
-      <label>
-        <div>Choose a password:</div>
-        <input type="text"/>
-      </label>
+    const data = (await axios.post(`/api/user`, this.state)).data
+    console.log(data)
+  }
+  render() {
+    return (
+      <div>
+        <Link to={urls.LOG_IN}>Already have an account?</Link>
 
-      {/* {process.env.REACT_APP_HCAPTCHA_SITEKEY &&
-        <HCaptcha
-          sitekey={process.env.REACT_APP_HCAPTCHA_SITEKEY}
-          onVerify={console.log}/>} */}
+        <div>
+          Step 1: Create an account<br/>
+          Step 2: Pay what you want
+        </div>
 
-      <button type="submit">Continue</button>
-    </form>
-  </div>
-)
+        <h2>Create an account</h2>
+        An account allows you to save your vocabulary progress and continue the game on other devices.
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <div>Choose a username:</div>
+            <input type="text"
+              value={this.state.username||''}
+              onChange={(e)=>this.handleChange('username',e)}/>
+          </label>
+          <label>
+            <div>What is your email? (optional):</div>
+            <input type="text"
+              value={this.state.email||''}
+              onChange={(e)=>this.handleChange('email',e)}/>
+          </label>
+          <label>
+            <div>Choose a password:</div>
+            <input type="password"
+              value={this.state.password||''}
+              onChange={(e)=>this.handleChange('password',e)}/>
+          </label>
+
+          {/* {process.env.REACT_APP_HCAPTCHA_SITEKEY &&
+            <HCaptcha
+              sitekey={process.env.REACT_APP_HCAPTCHA_SITEKEY}
+              onVerify={console.log}/>} */}
+
+          <button type="submit">Continue</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default Form
