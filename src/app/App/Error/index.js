bd88cbad7b9ac720829b230f161cdbd5e29eac8c
Egill <egill@egill.xyz>
2021-08-22 20:01:25 +0000

    no message

--- src/app/App/Error/index.js
+++ src/app/App/Error/index.js
@@ -1,12 +1,12 @@
 import store from "app/App/store";
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import messages from "./messages";
 import Analytics from "app/Analytics";
 
 const Notification = (props) => {
   if (!props.error) return null;
-  let message = props.error.message;
+  let { message } = props.error;
   if (message in messages) {
     message = messages[message];
   }
