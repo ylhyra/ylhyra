f7fcb9c7c3777f123efa1842d758b04b55b6a87c
Egill <egill@egill.xyz>
2021-09-06 10:00:01 +0000

    wip

--- src/app/app/functions/log.js
+++ src/app/app/functions/log.js
@@ -4,15 +4,13 @@ import { isBrowser } from "app/app/functions/isBrowser";
  * Helper function for debugging
  */
 export const log = (...items) => {
-  items.forEach((item) => {
-    if (typeof item === "string" && isBrowser) {
-      console.log("%c " + item, "color: gray");
-    } else {
-      console.log(item);
-    }
-  });
+  if (isDev) {
+    items.forEach((item) => {
+      if (typeof item === "string" && isBrowser) {
+        console.log("%c " + item, "color: gray");
+      } else {
+        console.log(item);
+      }
+    });
+  }
 };
-
-// if() {
-//
-// }
