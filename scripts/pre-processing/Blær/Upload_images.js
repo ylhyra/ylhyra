71c82bead3bfa6892a6583b14b7bbd788ae7f520
Egill <egill@egill.xyz>
2021-09-15 13:00:01 +0000

    wip

--- scripts/pre-processing/Blær/Upload_images.js
+++ scripts/pre-processing/Blær/Upload_images.js
@@ -1,22 +1,35 @@
-(function() {
-  let articleURL = 'https://blaer.is/grein/vinkonur';
-  let title = 'Blær – Vinkonur vors og blóma';
-  var txt = document.editform.wpTextbox1
-  var value = txt.value
-  let split = value.split(/(https?:\/\/blaer.is\/[^" ]+)/g)
+const upload_path = "not_data/files/images/blær/";
+const http = require("https");
+const fs = require("fs");
 
-  let targetLength = split.map((x, i) => (i % 2 !== 0)).filter(Boolean).length
-  let currentLength = 0
-  let oldToNewTitles = {}
+const file = fs.createWriteStream("file.jpg");
+const request = http.get(
+  "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg",
+  function (response) {
+    response.pipe(file);
+  }
+);
+
+(function () {
+  let articleURL = "https://blaer.is/grein/fyrst-vid-erum-herna";
+  let title = "Blær – Fyrst við erum hérna";
+  var value = txt.value;
+  let split = value.split(/(https?:\/\/blaer.is\/[^" ]+)/g);
+
+  let targetLength = split.map((x, i) => i % 2 !== 0).filter(Boolean).length;
+  let currentLength = 0;
+  let oldToNewTitles = {};
 
   function checkIfDone() {
     if (currentLength === targetLength) {
-      const output = split.map((fileURL, index) => {
-        if (index % 2 === 0) return fileURL;
-        return oldToNewTitles[fileURL] || fileURL;
-      }).join('')
+      const output = split
+        .map((fileURL, index) => {
+          if (index % 2 === 0) return fileURL;
+          return oldToNewTitles[fileURL] || fileURL;
+        })
+        .join("");
       // console.log(output)
-      txt.value = output
+      txt.value = output;
     }
   }
 
@@ -25,27 +38,24 @@
     // console.log(fileURL)
     // return 'haha'
 
-    var api = new mw.Api()
-    api.postWithToken('csrf', {
-      filename: title + ' ' + Math.round(Math.random() * 100000),
-      text: `{{c}} Blær – ${articleURL}`,
-      url: fileURL,
-      action: 'upload',
-      ignorewarnings: '1',
-      format: 'json'
-    }).done(function(data) {
-      const newFileURL = `https://ylhyra.is/Special:Filepath/${data.upload.filename}`
-      oldToNewTitles[fileURL] = newFileURL
-      currentLength++
-      console.log(`Uploaded ${newFileURL}. Done ${currentLength} of ${targetLength}`)
-      checkIfDone()
-    }).fail(function(error) {
-      if (error === 'fileexists-no-change') return;
-      console.error(`Could not upload ${fileURL}`)
-      console.error(error)
-      oldToNewTitles[fileURL] = fileURL
-      currentLength++
-      checkIfDone()
-    })
-  })
-})()
+    var api = new mw.Api();
+    api
+      .postWithToken("csrf", {
+        filename: title + " " + Math.round(Math.random() * 100000),
+        text: `{{c}} Blær – ${articleURL}`,
+        url: fileURL,
+        action: "upload",
+        ignorewarnings: "1",
+        format: "json",
+      })
+      .done(function (data) {
+        const newFileURL = `https://ylhyra.is/Special:Filepath/${data.upload.filename}`;
+        oldToNewTitles[fileURL] = newFileURL;
+        currentLength++;
+        console.log(
+          `Uploaded ${newFileURL}. Done ${currentLength} of ${targetLength}`
+        );
+        checkIfDone();
+      });
+  });
+})();
