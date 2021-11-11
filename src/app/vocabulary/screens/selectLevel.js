19c9ba2031cd236604ace339a1834a8d33da33b9
Egill <egill@egill.xyz>
2021-11-11 11:00:01 +0000

    wip

--- src/app/vocabulary/screens/selectLevel.js
+++ src/app/vocabulary/screens/selectLevel.js
@@ -10,43 +10,46 @@ export default () => (
     <div id="vocabulary-screen-inner">
       <div id="vocabulary-header">
         <button className="link" onClick={exitVocabularyScreen}>
-          Quit
+          Back
         </button>
       </div>
-      <div id="select-level-screen-header">
-        What is your current level in Icelandic?
+      <div id="game-container">
+        <div id="select-level-screen-header">
+          What is your current <br />
+          level in Icelandic?
+        </div>
+        <div id="select-level-buttons">
+          <button onClick={() => {}} className="green">
+            <h3>
+              Beginner <span>(A0)</span>
+            </h3>
+          </button>
+          <button onClick={() => {}} className="gray">
+            <h3>
+              Novice <span>(A1–A2)</span>
+            </h3>
+            <div>I can write simple sentences</div>
+          </button>
+          <button onClick={() => {}} className="gray">
+            <h3>
+              Intermediate <span>(B1–B2)</span>
+            </h3>
+            <div>
+              I can easily hold a conversation <br />
+              and I know most inflection tables
+            </div>
+          </button>
+          <button onClick={() => {}} className="gray">
+            <h3>
+              Advanced <span>(B2+)</span>
+            </h3>
+            <div>I can write nearly error-free text</div>
+          </button>
+        </div>
+        <div className="smaller gray center">
+          You can change this setting later
+        </div>
       </div>
-      <div id="select-level-buttons">
-        <button onClick={() => {}} className="green">
-          <h3>
-            Complete beginner <span>(A0)</span>
-          </h3>
-        </button>
-        <button onClick={() => {}} className="gray">
-          <h3>
-            Novice <span>(A1–A2)</span>
-          </h3>
-          <div>I can not hold a conversation</div>
-        </button>
-        <button onClick={() => {}} className="gray">
-          <h3>
-            Intermediate <span>(B1–B2)</span>
-          </h3>
-          <div>
-            I can hold a conversation but I often make grammatical errors
-          </div>
-        </button>
-        <button onClick={() => {}} className="gray">
-          <h3>
-            Advanced <span>(C1–C2)</span>
-          </h3>
-          <div>
-            I can write near-perfect text but I would like to improve my use of
-            idiomatic phrases.
-          </div>
-        </button>
-      </div>
-      You can change this setting later
     </div>
   </div>
 );
