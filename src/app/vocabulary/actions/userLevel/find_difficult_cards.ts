39bfea6470d7845ed8d052fdaa5e256e84ebef2d
Egill <egill@egill.xyz>
2021-11-14 19:00:00 +0000

    wip

--- src/app/vocabulary/actions/userLevel/find_difficult_cards.ts
+++ src/app/vocabulary/actions/userLevel/find_difficult_cards.ts
@@ -1,11 +1,16 @@
 import { getUserLevel } from "app/vocabulary/actions/userLevel/index";
 import { CardId } from "app/vocabulary/actions/card/types";
-import { getCardLevel, getLevel } from "app/vocabulary/actions/card/card_data";
 import {
-  USER_LEVEL_BEGINNER,
-  USER_LEVEL_NOVICE,
-  USER_LEVEL_INTERMEDIATE,
-  USER_LEVEL_ADVANCED,
+  getCardLevel,
+  getDifficulty,
+  getImportance,
+  getLevel,
+} from "app/vocabulary/actions/card/card_data";
+import {
+  userLevel_BEGINNER,
+  userLevel_NOVICE,
+  userLevel_INTERMEDIATE,
+  userLevel_ADVANCED,
   B1,
   A1,
   B2,
@@ -16,20 +21,20 @@ import {
 } from "app/vocabulary/constants";
 
 const difficultyLevelToCEFR = {
-  [USER_LEVEL_BEGINNER]: A1,
-  [USER_LEVEL_NOVICE]: A1,
-  [USER_LEVEL_INTERMEDIATE]: B1,
-  [USER_LEVEL_ADVANCED]: B2,
+  [userLevel_BEGINNER]: A1,
+  [userLevel_NOVICE]: A1,
+  [userLevel_INTERMEDIATE]: B1,
+  [userLevel_ADVANCED]: B2,
 };
 
-const difficultToUserLevel = [
-  { user_level: USER_LEVEL_BEGINNER, difficulty: DIFFICULT_FOR_BEGINNERS },
-  { user_level: USER_LEVEL_NOVICE, difficulty: DIFFICULT_FOR_BEGINNERS },
+const cardDifficultyToUserLevel = [
+  { userLevel: userLevel_BEGINNER, difficulty: DIFFICULT_FOR_BEGINNERS },
+  { userLevel: userLevel_NOVICE, difficulty: DIFFICULT_FOR_BEGINNERS },
   {
-    user_level: USER_LEVEL_INTERMEDIATE,
+    userLevel: userLevel_INTERMEDIATE,
     difficulty: DIFFICULT_FOR_INTERMEDIATE,
   },
-  { user_level: USER_LEVEL_ADVANCED, difficulty: DIFFICULT_FOR_ADVANCED },
+  { userLevel: userLevel_ADVANCED, difficulty: DIFFICULT_FOR_ADVANCED },
 ];
 
 export const howDifficultIsCard = (id: CardId): number => {
@@ -42,16 +47,20 @@ export const howDifficultIsCard = (id: CardId): number => {
 
   let outputDifficulty = 0;
 
-  switch (cardDifficulty) {
-    case DIFFICULT_FOR_BEGINNERS:
-    case DIFFICULT_FOR_INTERMEDIATE:
-    case DIFFICULT_FOR_ADVANCED:
+  if (cardDifficulty) {
+    const indexOfCardDifficulty = cardDifficultyToUserLevel.findIndex(
+      (j) => j.difficulty === cardDifficulty
+    );
+    const indexOfUserLevel = cardDifficultyToUserLevel.findIndex(
+      (j) => j.userLevel === userLevel
+    );
   }
+
   // switch (userLevel) {
-  //   case USER_LEVEL_BEGINNER:
-  //   case USER_LEVEL_NOVICE:
-  //   case USER_LEVEL_INTERMEDIATE:
-  //   case USER_LEVEL_ADVANCED:
+  //   case userLevel_BEGINNER:
+  //   case userLevel_NOVICE:
+  //   case userLevel_INTERMEDIATE:
+  //   case userLevel_ADVANCED:
   // }
 };
 
