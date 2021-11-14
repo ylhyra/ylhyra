a383b34da08a414e18cdb3524b6a3e39e7a78a86
Egill <egill@egill.xyz>
2021-11-14 17:00:01 +0000

    wip

--- src/app/vocabulary/actions/userLevel/find_difficult_cards.ts
+++ src/app/vocabulary/actions/userLevel/find_difficult_cards.ts
@@ -1,15 +1,3 @@
-// export const isBelowEasinessLevel = (id: CardId) => {
-//   return isEasinessLevelOn() && getSortKey(id) < getEasinessLevel();
-// };
-//
-// export const getSortKeyAdjustedForEasinessLevel = (id: CardId) => {
-//   return getSortKeyAdjusted(id, getEasinessLevel());
-// };
-//
-// export const getSortKeyAdjusted = (j) => {
-//   return getSortKey(id) > j ? getSortKey(id) : 100000 - getSortKey(id);
-// };
-
 import { getUserLevel } from "app/vocabulary/actions/userLevel/index";
 import { CardId } from "app/vocabulary/actions/card/types";
 import { getCardLevel, getLevel } from "app/vocabulary/actions/card/card_data";
@@ -22,6 +10,9 @@ import {
   A1,
   B2,
   A2,
+  DIFFICULT_FOR_BEGINNERS,
+  DIFFICULT_FOR_INTERMEDIATE,
+  DIFFICULT_FOR_ADVANCED,
 } from "app/vocabulary/constants";
 
 const difficultyLevelToCEFR = {
@@ -31,10 +22,31 @@ const difficultyLevelToCEFR = {
   [USER_LEVEL_ADVANCED]: B2,
 };
 
+const difficultToUserLevel = [
+  { user_level: USER_LEVEL_BEGINNER, difficulty: DIFFICULT_FOR_BEGINNERS },
+  { user_level: USER_LEVEL_NOVICE, difficulty: DIFFICULT_FOR_BEGINNERS },
+  {
+    user_level: USER_LEVEL_INTERMEDIATE,
+    difficulty: DIFFICULT_FOR_INTERMEDIATE,
+  },
+  { user_level: USER_LEVEL_ADVANCED, difficulty: DIFFICULT_FOR_ADVANCED },
+];
+
 export const howDifficultIsCard = (id: CardId): number => {
   /* If unseen (?) */
   const cardLevel = getCardLevel(id);
   const userLevel = getUserLevel();
+
+  const cardDifficulty = getDifficulty(id);
+  const cardImportance = getImportance(id);
+
+  let outputDifficulty = 0;
+
+  switch (cardDifficulty) {
+    case DIFFICULT_FOR_BEGINNERS:
+    case DIFFICULT_FOR_INTERMEDIATE:
+    case DIFFICULT_FOR_ADVANCED:
+  }
   // switch (userLevel) {
   //   case USER_LEVEL_BEGINNER:
   //   case USER_LEVEL_NOVICE:
