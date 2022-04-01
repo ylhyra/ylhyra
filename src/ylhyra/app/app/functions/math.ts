89c3cf8dda0bbbe768a6cd9220e5c0c41b624e84
Egill <egill@egill.xyz>
2022-04-01 21:07:10 +0000

    no message

--- src/ylhyra/app/app/functions/math.ts
+++ src/ylhyra/app/app/functions/math.ts
@@ -1,11 +1,11 @@
-import { seconds } from "modules/time";
+import { Milliseconds, seconds } from "modules/time";
 
-export const average = (arr = []) => {
+export const average = (arr: number[] = []) => {
   if (arr.length === 0) return 0;
   return arr.reduce((a, b) => a + b, 0) / arr.length;
 };
 
-export const clamp = function (input, min, max) {
+export const clamp = function (input: number, min: number, max: number) {
   return minIgnoreUndef(maxIgnoreUndef(input, min), max);
 };
 
@@ -16,6 +16,13 @@ export const mapValueToRange = ({
   output_from,
   output_to,
   ...options
+}: {
+  value: number;
+  input_from: number;
+  input_to: number;
+  output_from: number;
+  output_to: number;
+  clamp?: boolean;
 }) => {
   const slope = (output_to - output_from) / (input_to - input_from);
   const output = output_from + slope * (value - input_from);
@@ -25,44 +32,48 @@ export const mapValueToRange = ({
   return output;
 };
 
-export const minIgnoreFalsy = (...values) => {
+export const minIgnoreFalsy = (...values: any[]) => {
   return minIgnoreUndef(...values.filter(Boolean));
 };
 
-export const maxIgnoreFalsy = (...values) => {
+export const maxIgnoreFalsy = (...values: any[]) => {
   return maxIgnoreUndef(...values.filter(Boolean));
 };
 
-export const minIgnoreUndef = (...values) => {
-  const j = Math.min(...values.filter((i) => i !== undefined && i !== null));
+export const minIgnoreUndef = (...values: (number | undefined | null)[]) => {
+  const j = Math.min(
+    ...(values.filter((i) => i !== undefined && i !== null) as number[])
+  );
   return j !== Infinity ? j : null;
 };
 
-export const maxIgnoreUndef = (...values) => {
-  const j = Math.max(...values.filter((i) => i !== undefined && i !== null));
+export const maxIgnoreUndef = (...values: (number | undefined | null)[]) => {
+  const j = Math.max(
+    ...(values.filter((i) => i !== undefined && i !== null) as number[])
+  );
   return j !== -Infinity ? j : null;
 };
 
 /**
  * Randomly adds or subtracts up to 10% of the input
  */
-export const addSomeRandomness = (input, amount = 0.1) => {
+export const addSomeRandomness = (input: number, amount = 0.1) => {
   return input + input * amount * (Math.random() - 0.5) * 2;
 };
 
-export const roundToInterval = (v, roundBy) => {
+export const roundToInterval = (v: number, roundBy: number) => {
   return Math.round(v / roundBy) * roundBy;
 };
 
-export const roundMsToSec = (v) => {
+export const roundMsToSec = (v: Milliseconds) => {
   return roundToInterval(v, seconds);
 };
 
-export const roundMsTo100Sec = (v) => {
+export const roundMsTo100Sec = (v: Milliseconds) => {
   return roundToInterval(v, 100 * seconds);
 };
 
-export const toFixedFloat = (input, f) => {
+export const toFixedFloat = (input: number, f: number) => {
   return parseFloat(input.toFixed(f));
 };
 
@@ -74,6 +85,11 @@ export const mapZeroToInfinityToZeroToOne = ({
   goal_input,
   goal_output,
   pow = 1.4,
+}: {
+  input: number;
+  goal_input: number;
+  goal_output: number;
+  pow: number;
 }) => {
   input **= pow;
   goal_input **= pow;
