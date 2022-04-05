fcd8eb404c098f2e9afebfe0e8a710a13eb4e457
Egill <egill@egill.xyz>
2022-04-05 16:04:19 +0000

    no message

--- src/app/elements/layout/Footer.js
+++ src/app/elements/layout/Footer.js
@@ -18,21 +18,13 @@ export default () => (
         <span className="footer-gray">
           <a href="mailto:ylhyra@ylhyra.is">Report&nbsp;errors</a>
         </span>
-        {/* •
-      <span className="footer-gray">
-        <Link href="/Project:Become_a_collaborator">Collaborate</Link>
-      </span> */}
-        <span className="pwyw-on">
-          •
-          <span className="footer-gray">
-            <Link href="/donate">Donate</Link>
-          </span>
-        </span>
-        {/* • */}
+        {/*<span className="pwyw-on">*/}
+        {/*  •*/}
+        {/*  <span className="footer-gray">*/}
+        {/*    <Link href="/donate">Donate</Link>*/}
+        {/*  </span>*/}
+        {/*</span>*/}
       </span>
-      {/* <span className="footer-gray">
-    <a href="/Special:Search">Search</a>
-    </span> */}
     </div>
   </div>
 );
