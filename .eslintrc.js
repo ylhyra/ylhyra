f20ee25ac8b79a0b3231a34747f8badf86fa7a6b
Egill <egill@egill.xyz>
2022-04-27 14:36:40 +0000

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       files: ["*.ts", "*.tsx"],
       parserOptions: {
-        project: "./tsconfig.json",
+        project: "./tsconfig_strict.json",
         tsconfigRootDir: __dirname,
       },
       rules: {
