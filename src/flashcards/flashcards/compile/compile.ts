36577cf286e65049548c03114ccf8b067f223ea2
Egill <egill@egill.xyz>
2022-05-04 22:00:00 +0000

    wip

--- src/flashcards/flashcards/compile/compile.ts
+++ src/flashcards/flashcards/compile/compile.ts
@@ -4,12 +4,16 @@ import {
   Direction,
   UnprocessedDeck,
 } from "flashcards/flashcards/types/types";
+import { entries } from "modules/typescript/objectEntries";
 
 export const compileDeck = (deck: UnprocessedDeck): DeckProcessed => {
   let cards: DeckProcessed["cards"] = {};
   let terms: DeckProcessed["terms"] = {};
   let dependencies: DeckProcessed["dependencies"] = {};
   let alternativeIds: DeckProcessed["alternativeIds"] = {};
+  entries(deck.rows).forEach(([rowId, row]) => {
+    compileRow(row);
+  });
   return { cards, terms, dependencies, alternativeIds };
 };
 
@@ -33,8 +37,15 @@ export const compileRow = (row: Row) => {
   if (row.direction === "BOTH" || row.direction === "FRONT_TO_BACK") {
     cardIds.push(createCardId(row.rowId, "FRONT_TO_BACK"));
   }
+  if (row.direction === "BOTH" || row.direction === "BACK_TO_FRONT") {
+    cardIds.push(createCardId(row.rowId, "BACK_TO_FRONT"));
+  }
 };
 
+/**
+ * A CardId encodes both the rowId and the direction
+ * Todo: Is this the best method?
+ */
 export const createCardId = (rowId: RowId, direction: Direction): CardId => {
   return `${rowId}-${direction}` as CardId;
 };
