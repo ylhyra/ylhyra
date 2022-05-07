06f3f546a0d0e50976ae90db1ddecbb01e92a188
Egill <egill@egill.xyz>
2022-05-07 11:51:32 +0000

    no message

--- scripts/fixImports2.sh
+++ scripts/fixImports2.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+{
+    sleep 3
+    kill $$
+} &
+
 # Webstorm - run importjs on file save
 # Install https://plugins.jetbrains.com/plugin/7177-file-watchers/
+# npm i -g import-js
+#
+# Edit the following file:
+#   > /usr/local/lib/node_modules/import-js/build/importjs.js
+# Change
+#     if (process.stdin.isTTY)
+# to
+#     if (true)
 #
 # Settings:
-#    Script: $ProjectFileDir$/scripts/fixImports2.sh
+#    Program: $ProjectFileDir$/scripts/fixImports2.sh
 #    Args: $FilePathRelativeToProjectRoot$
 
-#npx import-js fix --overwrite "$1"
-#npx prettier "$1" --write
-
-echo "$1"
+cat "$1" | npx import-js fix --overwrite "$1"
+npx prettier "$1" --write
+exit 0
