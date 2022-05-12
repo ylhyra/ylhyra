7ad8f177a611e9e8e78d28e8360ae890fcbb0f85
Egill <egill@egill.xyz>
2022-05-12 10:18:52 +0000

    no message

--- src/flashcards/flashcards/tmp_test.ts
+++ src/flashcards/flashcards/tmp_test.ts
@@ -1,22 +1,52 @@
+// @ts-nocheck
+
 /*
    ts-node src/flashcards/flashcards/tmp_test.ts
  */
 
+import { makeAutoObservable } from "mobx";
+
 /**
  * Tilraun: Eru prototypur betri en classar?
  */
-class row2 {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
+(() => {
+  let deck = [];
+  class row2 {
+    name: string;
+    constructor() {
+      makeAutoObservable(this);
+    }
+    setName(name: string) {
+      this.name = name;
+    }
   }
-}
 
-const start = performance.now();
+  const start = performance.now();
+  for (let i = 0; i < 100000; i++) {
+    const X = new row2();
+    X.setName("Hello");
+    deck.push(X);
+  }
+  const time = performance.now() - start;
+  console.log(`Class took ${Math.round(time)} milliseconds`);
+})();
 
-for (let i = 0; i < 100000; i++) {
-  new row2("test" + i);
-}
-
-const time = performance.now() - start;
-console.log(`Took ${Math.round(time)} milliseconds`);
+(() => {
+  let deck = [];
+  const newRow = () => {
+    return {
+      name: "",
+      setName: (name: string) => {
+        this.name = name;
+      },
+    };
+  };
+  const start = performance.now();
+  for (let i = 0; i < 100000; i++) {
+    const X = newRow();
+    X.setName("Hello");
+    deck.push(X);
+  }
+  const time = performance.now() - start;
+  console.log(`Static object took ${Math.round(time)} milliseconds`);
+})();
