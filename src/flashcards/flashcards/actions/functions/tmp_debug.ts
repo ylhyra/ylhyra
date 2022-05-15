1b06ed53ba314be1ce64b922f08430394200613b
Egill <egill@egill.xyz>
2022-05-15 15:00:01 +0000

    wip

---
+++ src/flashcards/flashcards/actions/functions/tmp_debug.ts
@@ -0,0 +1,9 @@
+/**
+ * Tilraun til að sjá flamegraph og svoleis
+ *
+ * https://www.jetbrains.com/help/webstorm/v8-cpu-and-memory-profiling.html#node_profiling_before_you_start
+ */
+
+import { setupTestSession } from "flashcards/flashcards/actions/functions/testSetup";
+
+setupTestSession();
