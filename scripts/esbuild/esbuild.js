8ff3389f396c74838e50d48a07dfcae48d9d0c5e
Egill <egill@egill.xyz>
2022-06-28 21:00:00 +0000

    wip

--- scripts/esbuild/esbuild.js
+++ scripts/esbuild/esbuild.js
@@ -1,18 +1,19 @@
 const esbuild = require("esbuild");
-const { nodeExternalsPlugin } = require("esbuild-node-externals");
+// const { nodeExternalsPlugin } = require("esbuild-node-externals");
 const ignorePlugin = require("esbuild-plugin-ignore");
-const argvFactory = require("minimist");
-const argv = argvFactory(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 
 esbuild.build({
-  entryPoints: ["src/ylhyra/server/index.ts"],
+  entryPoints: [argv.entry || "src/ylhyra/server/index.ts"],
   nodePaths: ["src"],
   bundle: true,
   platform: "node",
-  outfile: "build/server/ylhyra_server.js",
+  outfile: argv.out || "build/server/ylhyra_server.js",
   sourcemap: true,
+  external: ["./node_modules/*"],
+
   plugins: [
-    nodeExternalsPlugin(),
+    // nodeExternalsPlugin(),
     ignorePlugin([
       {
         resourceRegExp: /.styl$/,
