675fc4a8c81a07e80068b6e37f9adee9270e465b
Egill <egill@egill.xyz>
2022-06-28 14:00:00 +0000

    wip

--- scripts/esbuild/esbuild.js
+++ scripts/esbuild/esbuild.js
@@ -1,7 +1,8 @@
-// /* eslint-disable */
 const esbuild = require("esbuild");
 const { nodeExternalsPlugin } = require("esbuild-node-externals");
 const ignorePlugin = require("esbuild-plugin-ignore");
+const argvFactory = require("minimist");
+const argv = argvFactory(process.argv.slice(2));
 
 esbuild.build({
   entryPoints: ["src/ylhyra/server/index.ts"],
