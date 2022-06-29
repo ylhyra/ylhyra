cae7a9bf8503fa8db94dc338459aa9e81ee4d12f
Egill <egill@egill.xyz>
2022-06-29 10:00:00 +0000

    wip

--- src/flashcards/app/app.tsx
+++ src/flashcards/app/app.tsx
@@ -1,7 +1,7 @@
 import { Layout } from "flashcards/app/layout/layout";
 import { Routes } from "flashcards/app/routes";
-import "flashcards/app/styles/output.css";
-import "flashcards/flashcards/styles/index.styl";
+import "flashcards/app/styles/index.styl";
+import "flashcards/app/styles/tailwind/output.css";
 import { configure as mobxConfigure } from "mobx";
 import React from "react";
 import ReactDOM from "react-dom";
