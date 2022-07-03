b12beccaf7c063be800e62c4025a3f729e81e846
Egill <egill@egill.xyz>
2022-07-03 18:00:00 +0000

    wip

--- src/flashcards/flashcards/flashcardsStore.ts
+++ src/flashcards/flashcards/flashcardsStore.ts
@@ -1,20 +1,16 @@
 import { Deck } from "flashcards/flashcards/actions/deck/deck";
 import { DeckId } from "flashcards/flashcards/types";
-import { initialize, saveFlashcardsStore } from "flashcards/sync/initialize";
+import { saveFlashcardsStore } from "flashcards/sync/initialize";
 import { makeAutoObservable, reaction } from "mobx";
 import { applyFunctionToEachObjectValue } from "modules/applyFunctionToEachObjectValue";
 import { isBrowser } from "modules/isBrowser";
 
-export class flashcardsStore {
+export class FlashcardsStore {
   decks: Record<DeckId, Deck> = {};
 
   constructor() {
     makeAutoObservable(this);
-
-    /** Auto-save (after initialization is complete) */
-    setTimeout(() => {
-      reaction(() => Object.keys(this.decks), saveFlashcardsStore);
-    }, 500);
+    reaction(() => Object.keys(this.decks), saveFlashcardsStore);
   }
 
   toJSON() {
@@ -26,8 +22,8 @@ export class flashcardsStore {
   }
 }
 
-const _store = new flashcardsStore();
-export const getFlashcardsStore = (): flashcardsStore => _store;
+const _store = new FlashcardsStore();
+export const getFlashcardsStore = (): FlashcardsStore => _store;
 
 if (isBrowser) {
   // @ts-ignore
--- src/flashcards/sync/initialize.ts
+++ src/flashcards/sync/initialize.ts
@@ -7,6 +7,7 @@ import { logDev } from "modules/log";
 import { entries } from "modules/typescript/objectEntries";
 import { warnIfFunctionIsSlow } from "modules/warnIfFunctionIsSlow";
 
+export let initialized = false;
 export const initialize = action(() => {
   try {
     const savedFlashcardsStore = getFromLocalStorage("decks");
@@ -16,13 +17,15 @@ export const initialize = action(() => {
         getFlashcardsStore().decks[deckId as DeckId] = new Deck(data);
       });
     }, "initializeFlashcardsStore");
+    setTimeout(() => (initialized = true), 0);
   } catch (e) {
     console.error(e);
-    console.error("Likely malformatted flashcards store data");
+    console.error("Likely malformed flashcards store data");
   }
 });
 
 export function saveFlashcardsStore() {
+  if (!initialized) return;
   logDev("Flashcards store saved");
   saveInLocalStorage("decks", getFlashcardsStore().toJSON());
 }
